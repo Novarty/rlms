bc7a5d002b723713f260a0fe5276cb42920b33cb
novarty <artem.ivanov9616@gmail.com>
2018-05-14 22:16:50 +0300

    Added I18n support for teacher courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,11 +8,19 @@ class Course < ApplicationRecord
 
   validates :title, :slug, :description, :teacher_id, presence: true
   validates :title, :slug, :description, uniqueness: true
+  validate  :slug, :slug_course_validate
 
   scope :published, -> { where(published: true) }
   scope :not_published, -> { where(published: false) }
 
   scope :ordered_by_title, -> { order(title: :asc) }
+
+  WHITELIST = /^[a-zA-Z0-9_-]*$/
+  def slug_course_validate
+    unless WHITELIST=~ slug
+      errors[:slug] << I18n.t("slug.invalide")
+    end
+  end
 
   def to_param
     slug
